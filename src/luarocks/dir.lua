e1f426987d6d561e6cb78cfaeaf7027eda1efb2e
Peter Melnichenko <mpeterval@gmail.com>
2016-11-11 22:16:33 +0300

    Move dir.split_url and dir.normalize out of the core
    No longer used from the core

--- src/luarocks/dir.lua
+++ src/luarocks/dir.lua
@@ -24,4 +24,32 @@ function dir.dir_name(pathname)
    return (pathname:gsub("/*$", ""):match("(.*)[/]+[^/]*")) or ""
 end
 
+--- Split protocol and path from an URL or local pathname.
+-- URLs should be in the "protocol://path" format.
+-- For local pathnames, "file" is returned as the protocol.
+-- @param url string: an URL or a local pathname.
+-- @return string, string: the protocol, and the pathname without the protocol.
+function dir.split_url(url)
+   assert(type(url) == "string")
+   
+   local protocol, pathname = url:match("^([^:]*)://(.*)")
+   if not protocol then
+      protocol = "file"
+      pathname = url
+   end
+   return protocol, pathname
+end
+
+--- Normalize a url or local path.
+-- URLs should be in the "protocol://path" format. System independent
+-- forward slashes are used, removing trailing and double slashes
+-- @param url string: an URL or a local pathname.
+-- @return string: Normalized result.
+function dir.normalize(name)
+   local protocol, pathname = dir.split_url(name)
+   pathname = pathname:gsub("\\", "/"):gsub("(.)/*$", "%1"):gsub("//", "/")
+   if protocol ~= "file" then pathname = protocol .."://"..pathname end
+   return pathname
+end
+
 return dir
